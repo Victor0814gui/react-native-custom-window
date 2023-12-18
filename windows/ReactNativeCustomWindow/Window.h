6e7d08b94005e619a55bdb0a153fbf9c4d16490b
Victor Guilherme <92493696+Victor0814gui@users.noreply.github.com>
2023-12-18 15:02:07 -0300

    fix: add missing methods and correct syntax

--- windows/ReactNativeCustomWindow/Window.h
+++ windows/ReactNativeCustomWindow/Window.h
@@ -23,59 +23,113 @@ using namespace Windows::UI::Xaml::Controls;
 using namespace Windows::ApplicationModel::Core;
 
 
+namespace RN = winrt::Microsoft::ReactNative;
 
 namespace WindowRN {
     REACT_MODULE(WindowRNModule, L"window");
-    struct WindowRNModule final {  
-      
-        winrt::Microsoft::ReactNative::ReactContext context;
+    struct WindowRNModule final {
 
-        REACT_INIT(Initialize,L"init");
-        void Initialize(const winrt::Microsoft::ReactNative::ReactContext& reactContext) noexcept
+        RN::ReactContext context;
+
+        REACT_INIT(Initialize, L"init");
+        void Initialize(const RN::ReactContext& reactContext) noexcept
         {
-        context = reactContext;
-        }     
+            context = reactContext;
+        }
 
-        REACT_METHOD(Full, L"fullscreen");
-        void Full() noexcept
+        REACT_METHOD(Fullscreen, L"fullscreen")
+        void Fullscreen(RN::ReactPromise<void> promise) noexcept
         {
-            context.UIDispatcher().Post([] {
+            try {
+                auto jsDispatcher = context.JSDispatcher();
 
+                context.UIDispatcher().Post([promise, jsDispatcher] {
                     ApplicationView view = ApplicationView::GetForCurrentView();
-                    if (view.IsFullScreenMode())
+                    bool isFullScreen = view.IsFullScreenMode();
+                    if (isFullScreen)
                     {
                         view.ExitFullScreenMode();
-                        // The SizeChanged event will be raised when the exit from full screen mode is complete.
+                        jsDispatcher.Post([promise] { promise.Resolve(); });
                     }
                     else
                     {
-                        if (view.TryEnterFullScreenMode())
-                        {
-                            // The SizeChanged event will be raised when the entry to full screen mode is complete.
-                        }
-                        else
-                        {
-                        }
-                    }
-            });
-        }
-
-        REACT_METHOD(SetSize, L"setSize");
-        void SetSize(float  w, float  h) noexcept
-        {
-
-            context.UIDispatcher().Post([w,h] {        
-
-                ApplicationView view = ApplicationView::GetForCurrentView();
-                    if (view.TryResizeView({ w,h }))
-                    {
-                    }
-                    else
-                    {
+                        view.TryEnterFullScreenMode();
+                        jsDispatcher.Post([promise] { promise.Resolve(); });
                     }
                 });
+            }
+            catch (const hresult_error& ex) {
+                promise.Reject(RN::ReactError{ winrt::to_string(ex.message()).c_str() });
+            }
         }
 
+        REACT_METHOD(IsFullScreen, L"isFullScreen")
+        void IsFullScreen(RN::ReactPromise<bool> promise) noexcept
+        {
+            try {
+                auto jsDispatcher = context.JSDispatcher();
+                context.UIDispatcher().Post([jsDispatcher,promise] {
+                    ApplicationView view = ApplicationView::GetForCurrentView();
+                    bool isFullScreen = view.IsFullScreenMode();
+                    jsDispatcher.Post([promise, isFullScreen] { promise.Resolve(isFullScreen); });
+                });
+            }
+            catch (const hresult_error& ex) {
+                promise.Reject(RN::ReactError{ winrt::to_string(ex.message()).c_str() });
+            }
+        }
 
+        REACT_METHOD(EnterFullscreen, L"enterFullscreen")
+        void EnterFullscreen(RN::ReactPromise<void> promise) noexcept
+        {
+            try {
+                auto jsDispatcher = context.JSDispatcher();
+
+                context.UIDispatcher().Post([jsDispatcher, promise] {
+                    ApplicationView view = ApplicationView::GetForCurrentView();
+                    view.TryEnterFullScreenMode();
+                    jsDispatcher.Post([promise] { promise.Resolve(); });
+                });
+            }
+            catch (const hresult_error& ex) {
+                promise.Reject(RN::ReactError{ winrt::to_string(ex.message()).c_str() });
+            }
+        }
+
+        REACT_METHOD(LeaveFullscreen, L"leaveFullscreen")
+        void LeaveFullscreen(RN::ReactPromise<void> promise) noexcept
+        {
+            try {
+                auto jsDispatcher = context.JSDispatcher();
+                context.UIDispatcher().Post([jsDispatcher, promise] {
+                    ApplicationView view = ApplicationView::GetForCurrentView();
+                    view.ExitFullScreenMode();
+                    jsDispatcher.Post([promise] { promise.Resolve(); });
+                });
+            }
+            catch (const hresult_error& ex) {
+                promise.Reject(RN::ReactError{ winrt::to_string(ex.message()).c_str() });
+            }
+        }
+
+        REACT_METHOD(SetSize, L"setSize")
+        void SetSize(JSValueObject size, RN::ReactPromise<void> promise) noexcept
+        {
+            try {
+                auto jsDispatcher = context.JSDispatcher();
+
+                float width = size["width"].AsSingle();
+                float height = size["height"].AsSingle();
+                context.UIDispatcher().Post([jsDispatcher, width, height, promise] {
+
+                    ApplicationView view = ApplicationView::GetForCurrentView();
+                    view.TryResizeView({ width,height });
+                    jsDispatcher.Post([promise] { promise.Resolve(); });
+                });
+            }
+            catch (const hresult_error& ex) {
+                promise.Reject(RN::ReactError{ winrt::to_string(ex.message()).c_str() });
+            }
+        };
     };
 }
